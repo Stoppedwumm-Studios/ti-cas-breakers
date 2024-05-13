691c1d5cfae3f2e09517ab4825aff3b6d6871fec
Stoppedwumm <129097720+Stoppedwumm@users.noreply.github.com>
2024-05-13 13:32:26 +0200

    Create wtc.py

---
+++ src/wtc.py
@@ -0,0 +1,17 @@
+import time
+
+time.sleep(15) # Replace the number with the seconds you want it to freeze
+
+# After executing, the timer will freeze! You can hit
+# Ctrl/Strg+Alt+Delete/Entfernen
+# to get to the quick security menu
+# From there, open Task Manager and all windows functions are re-enabled without TI seeing that you are away
+
+
+# REMEMBER TO GET BACK TO TI BEFORE THE TIME RUNS OUT
+# How it works:
+#
+# Texas Instruments has made an mistake while porting python to TI CAS
+# When calling the sleep function, the python runtime blocks the main process
+# This includes the timer and the function to check if the user exited the program.
+# The python process is not affected and continues to execute. 
